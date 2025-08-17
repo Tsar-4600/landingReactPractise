1e85394842591921ddcc0e936531019807813bc4
Tsar-4600 <nikitabasalaef91@gmail.com>
2025-08-17 18:50:03 +0300

    AdvantagesServiceGallery

--- src/components/ServiceAdvantage/ServiceAdvantage.jsx
+++ src/components/ServiceAdvantage/ServiceAdvantage.jsx
@@ -1,47 +1,92 @@
-import {Heading, Box, Image, Grid, GridItem} from "@chakra-ui/react";
+import { Heading, Box, Image, Grid, GridItem,  Span} from "@chakra-ui/react";
 import Zoom from 'react-medium-image-zoom'
-import 'react-medium-image-zoom/dist/styles.css'
+
 
 function ServiceAdvantage() {
     return (
-        <>
-            <Heading as="h2" fontSize={{base: "1.5rem", md:"2rem", lg:"3rem"}} lineHeight={1}  textAlign={"right"} >
+
+        <Box>
+            <Heading
+                as="h2"
+                fontSize={{ base: "1.5rem", md: "2rem", lg: "3rem" }}
+                lineHeight={1}
+                textAlign="right"
+                mb={6}
+            >
                 Выездные Бригады
             </Heading>
-            <Grid templateColumns={{base: "repeat(4, 1fr)"}} templateRows={{base: "repeat(2, 1fr)"}}  >
-                <GridItem  colSpan={1} rowSpan={2} >
-                    <Zoom>
-                        <Image objectFit={"cover"} src="/img/default.jpg" />
-                    </Zoom>
+            <Grid templateColumns={{base: "1, 1fr", md: "repeat(2, 1fr)"}} gap={5}>
+                <GridItem>
+                    <Span>Ваша техника — под надежным контролем! Регулярное техобслуживание и оперативная помощь при неожиданных поломках — мы обеспечиваем бесперебойную работу вашего оборудования. Предупредите проблемы до их появления или вызовите бригаду для срочного ремонта — мы всегда на связи!</Span>
+                   
                 </GridItem>
-                <GridItem colSpan={2} maxH="230px" overflow={"hidden"} >
-                    <Zoom>
-                        <Image objectFit="fill"  src="/img/default.jpg" />
-                    </Zoom>
+                <GridItem>
+                    <Span>Ваша техника — под надежным контролем! Регулярное техобслуживание и оперативная помощь при неожиданных поломках — мы обеспечиваем бесперебойную работу вашего оборудования. Предупредите проблемы до их появления или вызовите бригаду для срочного ремонта — мы всегда на связи!</Span>
                 </GridItem>
-                <GridItem  maxH="230px">
-                    <Zoom>
-                        <Image  src="/img/default.jpg" />
-                    </Zoom>
-                </GridItem>
-                <GridItem colSpan={1} maxH="230px" >
-                    <Zoom>
-                        <Image  src="/img/default.jpg" />
-                    </Zoom>
-                </GridItem>
-                 <GridItem maxH="230px">
-                    <Zoom>
-                        <Image src="/img/default.jpg" />
-                    </Zoom>
-                </GridItem>
-                 <GridItem>
-                    <Zoom>
-                        <Image src="/img/default.jpg" />
-                    </Zoom>
-                </GridItem>
-             
             </Grid>
-        </>
+
+            <Grid
+                templateColumns={{ base: "repeat(4, 1fr)", md: "repeat(4, 1fr)" }}
+                templateRows={{ base: "repeat(5, 200px)", md: "repeat(2, 300px)" }}
+                gap={3}
+                width="100%"
+            >
+                {/* Основное вертикальное изображение (левый столбец) */}
+                <GridItem
+                    colSpan={{ base: 4, md: 1 }}
+                    rowSpan={{ base: 1, md: 2 }}
+                >
+                    <Image
+                        src="/img/default.jpg"
+                        width="100%"
+                        height="100%"
+                        objectFit="cover"
+                        alt="Выездная бригада"
+                    />
+                </GridItem>
+
+                {/* Первая горизонтальная пара изображений */}
+                <GridItem colSpan={{ base: 4, md: 2 }} rowSpan={1}>
+                    <Image
+                        src="/img/default.jpg"
+                        width="100%"
+                        height="100%"
+                        objectFit="cover"
+                        alt="Рабочий процесс"
+                    />
+                </GridItem>
+
+                <GridItem colSpan={{ base: 4, md: 1 }} rowSpan={2}>
+                    <Image
+                        src="/img/default.jpg"
+                        width="100%"
+                        height="100%"
+                        objectFit="cover"
+                        alt="Оборудование"
+                    />
+                </GridItem>
+
+                {/* Вторая горизонтальная пара изображений */}
+                <GridItem colSpan={{ base: 4, md: 1 }} rowSpan={1}>
+                    <Image
+                        src="/img/default.jpg"
+                        width="100%"
+                        height="100%"
+                        objectFit="cover"
+                        alt="Специалисты"
+                    />
+                </GridItem>
+                <GridItem colSpan={{ base: 4, md: 1 }} rowSpan={1}>
+                    <Image
+                        src="/img/default.jpg"
+                        width="100%"
+                        height="100%"
+                        objectFit="cover"
+                        alt="Специалисты"
+                    />
+                </GridItem>
+            </Grid>
+        </Box>
 
     )
 }
